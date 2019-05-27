a231e30357da1a87e53a8fdda414f9cbef58eeab
surya <surya@uxgorilla.com>
2019-05-27 17:41:10 +0530

    if else statement

---
+++ if.py
@@ -0,0 +1,13 @@
+>>> x = int(input("please enter the integer no"))
+please enter the integer no 6
+>>> x
+6
+>>> if x<0:
+...     print("surya")
+... elif x ==6:
+...     print("mohan")
+... else:
+...     print("arya")
+... 
+mohan
+
