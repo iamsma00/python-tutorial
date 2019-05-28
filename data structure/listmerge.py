8b50e2010aad29bdb3890650a3796fc1c9bbf49b
surya <surya@uxgorilla.com>
2019-05-28 12:41:18 +0530

    merging two list with single line code as well as loop statement

--- data structure/listmerge.py
+++ data structure/listmerge.py
@@ -1,1 +1,11 @@
-print([(x,y) for x in [1,2,3] for y in [2,4,5]])
+# single line code
+print([(x,y) for x in [1,2,3] for y in [2,4,5] if x!=y])
+
+# using for loop and if statement
+merge = []
+for x in range(1,4):
+    for y in [2,4,5]:
+                 if x!=y:
+                    merge.append((x,y))
+
+print(merge) 
