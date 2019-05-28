b76e9cbe3955ec9a492f6ba4630eb7b169328a4e
surya <surya@uxgorilla.com>
2019-05-28 14:50:33 +0530

    single line code and using loop

--- data structure/row.py
+++ data structure/row.py
@@ -6,3 +6,10 @@ matrix = [
 ]
 
 print([[row[i] for row in matrix] for i in range(4)])
+
+
+# using loop
+transposed = []
+for i in range(4):
+	transposed.append([row[i] for row in matrix])
+print(transposed)
