65f48925983ffba7ca3926ad33ba2dafcbe82c57
surya <surya@uxgorilla.com>
2019-05-28 15:53:23 +0530

    key value in dictionary and index value in sequence

--- data structure/dictionary.py
+++ data structure/dictionary.py
@@ -1,3 +1,5 @@
+# for fetching key value while looping in dictionaries
+
 detail = {'name':'surya',
 		  'age':22,
 		  'contact': 3455,
@@ -6,3 +8,8 @@ detail = {'name':'surya',
 
 for k,v in detail.items():
 	print(k,v)
+
+
+# for fetching index while looping in sequence
+for i,j in enumerate(['surya','mohan','arya']):
+	print(i,j)
